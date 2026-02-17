79414072bcac6c980d9795345c4b6d1f49a95799
Nicolas Conde <ncondesalazar@gmail.com>
2026-02-17 12:50:41 -0500

    feat: add STATIC_ROOT configuration for production deployment

--- config/settings/base.py
+++ config/settings/base.py
@@ -113,6 +113,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Media files (User uploads)
